7d18724b42b198bead5de61b58386c432a013390
Jean-Baptiste Giraudeau <jb@giraudeau.info>
2018-10-06 18:08:41 +0200

    dataArgument: use visitor instead of instanceOf

--- processor-api/src/main/java/org/derive4j/processor/api/model/DataArgument.java
+++ processor-api/src/main/java/org/derive4j/processor/api/model/DataArgument.java
@@ -21,6 +21,7 @@ package org.derive4j.processor.api.model;
 import java.util.function.BiFunction;
 import javax.lang.model.type.ErrorType;
 import javax.lang.model.type.TypeMirror;
+import javax.lang.model.util.SimpleTypeVisitor8;
 import org.derive4j.Data;
 import org.derive4j.Derive;
 import org.derive4j.ExportAsPublic;
@@ -50,7 +51,17 @@ public abstract class DataArgument {
 
   @ExportAsPublic
   static DataArgument dataArgument(String fieldName, TypeMirror type) {
-    if (type instanceof ErrorType) {
+    if (new SimpleTypeVisitor8<Boolean, Object>() {
+      @Override
+      public Boolean visitError(ErrorType t, Object o) {
+        return true;
+      }
+
+      @Override
+      protected Boolean defaultAction(TypeMirror e, Object o) {
+        return false;
+      }
+    }.visit(type)) {
       throw new IllegalArgumentException("Type of " + fieldName + " is not valid: " + type);
     }
     return DataArguments.dataArgument0(fieldName, type);
